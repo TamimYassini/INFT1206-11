2f76c51024dfbf30eaa01cf9577f78c0388f27fd
TamimYassini <156445496+TamimYassini@users.noreply.github.com>
2024-04-07 22:10:53 -0400

    assingment4_part4_evilcircle

--- assignment4/part4/main-finished.js
+++ assignment4/part4/main-finished.js
@@ -35,3 +35,54 @@ class Ball extends shape {
     this.size = size;
     this.exists = exists;
   }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -(Math.abs(this.velX));
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = Math.abs(this.velX);
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -(Math.abs(this.velY));
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = Math.abs(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball)&& ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
+
+class EvilCricle extends shape {
+  constructor(x,y){
+    super(x,y,20,20);
+    this.colour ='white';
+    this.size = 10;
+
+  }
